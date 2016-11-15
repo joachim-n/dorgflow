6aa44a7a8d4dc9d76934f22738abc50d239d48f6
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-11-15 02:38:02 +0000

    branch list parser

--- Parser/GitBranchList.php
+++ Parser/GitBranchList.php
@@ -11,7 +11,7 @@ namespace Dorgflow\Parser;
  * @endcode
  */
 class GitBranchList {
-  
+
   protected $input;
 
   protected $branchList = [];
@@ -24,19 +24,33 @@ class GitBranchList {
 
   public function parse() {
     // Split the lines of the input.
-    $branch_list = explode("\n", $this->input);
-    print_r($branch_list);
-    
-    // Sort the lines by version number with the highest first. This accounts for a
+    $git_branch_list = explode("\n", $this->input);
+    print_r($git_branch_list);
+
+    $branch_names = [];
+    foreach ($git_branch_list as $branch) {
+      $matches = [];
+      preg_match("@^(?P<mark>.)\s+(?P<name>\S+)@", $branch, $matches);
+
+      if (!empty($matches['mark']) && $matches['mark'] == '*') {
+        $this->currentBranch = $matches['name'];
+      }
+      print_r(">$branch<");
+
+      // Build a clean list of branch names.
+      $branch_names[] = $matches['name'];
+    }
+
+    // Sort branch names by version number with the highest first. This accounts for a
     // situation where we're on a branch such as 7.x-3.x, and 7.x-2.x is a direct
     // ancestor because it's had no further development since branching.
-    usort($branch_list, 'version_compare');
-    $branch_list = array_reverse($branch_list);
-    
+    usort($branch_names, 'version_compare');
+
+    $this->branchList = array_reverse($branch_names);
   }
 
   public function get($property_name) {
-
+    return $this->{$property_name};
   }
 
 }
--- tests/GitWorkBranchTest.php
+++ tests/GitWorkBranchTest.php
@@ -21,29 +21,19 @@ class GitWorkBranchTest extends \PHPUnit_Framework_TestCase {
    * Test.
    */
   public function testGitWorkBranch() {
-    $branch_list = [
-      '* 12345-foo-bar',
-      '  67890',
-    ];
-    
-    
     $work_branch_parser = new \Dorgflow\Parser\GitBranchList();
-    
+
     $git_output = <<<EOT
-    master
-  * sandbox-oo
+  8.3.x
+* 12345-foo-bar
 EOT;
-    
-    
+
+
     $work_branch_parser->setInput($git_output);
     $work_branch_parser->parse();
-    
-    return;
 
-    $this->assertTrue($work_branch->exists(), 'A work branch was correctly found.');
-    $this->assertEquals('12345-foo-bar', $work_branch->getBranchName(), 'The branch name was found.');
-    $this->assertEquals('12345', $work_branch->getIssueNumber(), 'The issue number for the branch was found.');
-    $this->assertEquals('foo-bar', $work_branch->getIssueDescription(), 'The issue description for the branch was found.');
+
+    $this->assertEquals('12345-foo-bar', $work_branch_parser->get('currentBranch'), 'The current branch was found.');
   }
 
 }
