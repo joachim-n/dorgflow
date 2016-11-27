787b6647950944ba5824db57bb9df177da9f8291
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-11-27 08:58:53 +0000

    master branch knows if current

--- Waypoint/MasterBranch.php
+++ Waypoint/MasterBranch.php
@@ -6,7 +6,10 @@ class MasterBranch {
 
   protected $branchName;
 
+  protected $isCurrentBranch;
+
   function __construct(\Dorgflow\Situation $situation) {
+    // TODO: check order of the branch -- should be higher version numbers first!
     $branch_list = $situation->GitBranchList->getBranchList();
 
     foreach ($branch_list as $branch) {
@@ -28,13 +31,19 @@ class MasterBranch {
       // This should trigger a complete failure -- throw an exception!
       throw new \Exception("Can't find a master branch.");
     }
-    
+
+    $this->isCurrentBranch = ($situation->GitBranchList->getCurrentBranch() == $this->branchName);
+
     // Set this on the situation object, as other things depend on it.
     $situation->setMasterBranch($this);
   }
 
   public function getBranchName() {
     return $this->branchName;
+  }
+
+  public function isCurrentBranch() {
+    return $this->isCurrentBranch;
   }
 
 }
