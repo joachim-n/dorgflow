91488b12b793735d39bf4312668ba010bb383e82
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-11-27 15:40:36 +0000

    notes

--- Command/CreatePatch.php
+++ Command/CreatePatch.php
@@ -6,10 +6,13 @@ class CreatePatch {
   
   /*
     run the analyer to get stuff
+    - check git is clean
     - get the issue number from the branch
     - get the next comment number from the issue
     - make a patch
-    - figure out interdiff: what was the previous patch
+    - figure out interdiff: what was the previous patch.
+      get the patch list. get the commit for the most recent patch.
+      diff from that commit
   
   */
   
