bf98b20866b0361c063a9cb9630af83332f9c5a7
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-12-02 07:28:02 +0000

    added bail if no master branch

--- Command/LocalUpdate.php
+++ Command/LocalUpdate.php
@@ -19,6 +19,16 @@ class LocalUpdate {
 
     // Create waypoints.
     $master_branch = $situation->setUpMasterBranch();
+
+    // If the master branch is not current, abort.
+    // TODO: support updating an existing feature branch.
+    if (!$master_branch->isCurrentBranch()) {
+      print strtr("Detected master branch !branch, but it is not the current branch. Aborting.", [
+        '!branch' => $master_branch->getBranchName(),
+      ]);
+      exit();
+    }
+
     $feature_branch = $situation->setUpFeatureBranch();
 
     // Check whether feature branch exists.
