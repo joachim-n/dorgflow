72415e2f4ff80c270a04adf9495eae33badeb3fe
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-12-02 13:34:57 +0000

    started on create patch command

--- Command/CreatePatch.php
+++ Command/CreatePatch.php
@@ -2,18 +2,37 @@
 
 namespace Dorgflow\Command;
 
+use Dorgflow\Situation;
+
 class CreatePatch {
-  
-  /*
-    run the analyer to get stuff
-    - check git is clean
-    - get the issue number from the branch
-    - get the next comment number from the issue
-    - make a patch
-    - figure out interdiff: what was the previous patch.
-      get the patch list. get the commit for the most recent patch.
-      diff from that commit
-  
-  */
-  
+
+  public function execute() {
+    $situation = new Situation();
+
+    // Create branches.
+    $master_branch = $situation->setUpMasterBranch();
+    $feature_branch = $situation->setUpFeatureBranch();
+
+    // If the feature branch doesn't exist or is not current, abort.
+    if (!$feature_branch->exists()) {
+      throw new \Exception("Feature branch does not exist.");
+    }
+    if (!$feature_branch->isCurrentBranch()) {
+      throw new \Exception("Feature branch is not the current branch.");
+    }
+
+    /*
+      run the analyer to get stuff
+      - check git is clean
+      - get the issue number from the branch
+      - get the next comment number from the issue
+      - make a patch
+      - figure out interdiff: what was the previous patch.
+        get the patch list. get the commit for the most recent patch.
+        diff from that commit
+
+    */
+
+  }
+
 }
