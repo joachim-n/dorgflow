d49e917bc9aef46bb846b592a96bf07048729d34
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-12-02 07:31:05 +0000

    apply patches

--- Command/LocalUpdate.php
+++ Command/LocalUpdate.php
@@ -7,17 +7,9 @@ use Dorgflow\Situation;
 class LocalUpdate {
 
   public function execute() {
-    /*
-      run the analyer to get stuff
-      run over the waypoint objects:
-        - report whether it exists locally
-        - create itself if not.
-
-    */
-
     $situation = new Situation();
 
-    // Create waypoints.
+    // Create branches.
     $master_branch = $situation->setUpMasterBranch();
 
     // If the master branch is not current, abort.
@@ -30,6 +22,11 @@ class LocalUpdate {
     }
 
     $feature_branch = $situation->setUpFeatureBranch();
+
+    // Get the patches and create them.
+    // TODO: currently only the most recent patch is used.
+    $patches = $situation->setUpPatches();
+    //dump($patches);
 
     // Check whether feature branch exists.
     if ($feature_branch->exists()) {
@@ -44,6 +41,45 @@ class LocalUpdate {
       }
 
       $feature_branch->gitCreate();
+
+      print strtr("Feature branch !branch created.", [
+        '!branch' => $feature_branch->getBranchName(),
+      ]);
+    }
+
+    // If no patches, we're done.
+    if (empty($patches)) {
+      print "No patches to apply.";
+      return;
+    }
+
+    // Output the patches.
+    foreach ($patches as $patch) {
+      $patch_committed = $patch->commitPatch();
+
+      // Message.
+      if ($patch_committed) {
+        print strtr("Applied patch !patchname.", [
+          '!patchname' => $patch->getPatchFilename(),
+        ]);
+      }
+      else {
+        print strtr("Patch !patchname did not apply.", [
+          '!patchname' => $patch->getPatchFilename(),
+        ]);
+      }
+    }
+
+    // If final patch didn't apply, then output a message: the latest patch
+    // has rotted. Save the patch file to disk and give the filename in the
+    // message.
+    if (!$patch_committed) {
+      // Save the file so the user can apply it manually.
+      file_put_contents($patch->getPatchFilename(), $patch->getPatchFile());
+
+      print strtr("The most recent patch, !patchname, did not apply. You should attempt to apply it manually. The patch file has been saved to the working directory.", [
+        '!patchname' => $patch->getPatchFilename(),
+      ]);
     }
   }
 
