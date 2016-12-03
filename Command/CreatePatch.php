28918b0012465ff7e8af8a9bcbf05e3a64a9d457
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-12-03 15:44:40 +0000

    create patch: patch name, write patch!

--- Command/CreatePatch.php
+++ Command/CreatePatch.php
@@ -7,11 +7,13 @@ use Dorgflow\Situation;
 class CreatePatch {
 
   public function execute() {
-    $situation = new Situation();
+    $this->situation = new Situation();
+
+    // TODO: check git is clean!
 
     // Create branches.
-    $master_branch = $situation->setUpMasterBranch();
-    $feature_branch = $situation->setUpFeatureBranch();
+    $master_branch = $this->situation->setUpMasterBranch();
+    $feature_branch = $this->situation->setUpFeatureBranch();
 
     // If the feature branch doesn't exist or is not current, abort.
     if (!$feature_branch->exists()) {
@@ -21,18 +23,43 @@ class CreatePatch {
       throw new \Exception("Feature branch is not the current branch.");
     }
 
+    // TODO: get this from user input.
+    $sequential = FALSE;
+
+    // Select the diff command to use.
+    if ($sequential) {
+      $command = 'format-patch --stdout';
+    }
+    else {
+      $command = 'diff';
+    }
+
+    $master_branch_name = $master_branch->getBranchName();
+    $patch_name = $this->getPatchName($feature_branch);
+
+    shell_exec("git $command $master_branch_name > $patch_name");
+
+    drush_print_r("Written patch $patch_name with diff from $master_branch to local branch.");
+
+
     /*
-      run the analyer to get stuff
-      - check git is clean
-      - get the issue number from the branch
-      - get the next comment number from the issue
-      - make a patch
+    TODO:
       - figure out interdiff: what was the previous patch.
         get the patch list. get the commit for the most recent patch.
         diff from that commit
 
     */
 
+  }
+
+  protected function getPatchName($feature_branch) {
+    $issue_number = $this->situation->getIssueNumber();
+    $comment_number = $this->situation->DrupalOrgIssueNode->getNextCommentIndex();
+    $patch_number = "$issue_number-$comment_number";
+    $current_project = $this->situation->CurrentProject->getCurrentProjectName();
+    $branch_description = $feature_branch->getBranchDescription();
+
+    return "$patch_number.$current_project.$branch_description.patch";
   }
 
 }
