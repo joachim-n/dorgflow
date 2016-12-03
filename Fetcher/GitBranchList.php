643c034eb30748056625862fd1074aa85b797fed
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-12-03 16:07:47 +0000

    ponderings

--- Fetcher/GitBranchList.php
+++ Fetcher/GitBranchList.php
@@ -7,11 +7,31 @@ use Dorgflow\Parser\GitBranchList as GitBranchListParser;
 // Fetchers lazy load and cache in themselves!
 // Fetchers are responsible for one source of data, though they may provide
 // more than one piece of data from it.
+/*
+argh, which way round should this be? fethcer or parser on the outside?
+for tests, I want to isolate the parser.
+So I want a parser constructed without a fetcher, or with the fetcher I specify...
+
+and when should fetching happen -- on first call, or on construct? how does on construct
+affect testing? TRY WRITING SOME TESTS TO SEE!
+
+also, higher-level tests will need to use more stuff, and cut out the fetchers.
+which suggests fetchers are the last piece.
+
+
+
+
+
+*/
 class GitBranchList {
   
   protected $parser_class = GitBranchListParser::class;
   
   protected $parser = NULL;
+  
+  // TODO: might as well fetch data on creation, as if you create this it's 
+  // because you want something from it.
+  // but will that mess up testing?
 
   public function getBranchList() {
     if (!isset($this->parser)) {
