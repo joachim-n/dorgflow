4b64adae87ff77bcd65945cd494ec9f2aafc4d3e
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-12-06 22:57:18 +0000

    d.org node source

---
+++ DataSource/Fetcher/DrupalOrgIssueNode.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Dorgflow\DataSource\Fetcher;
+
+use Dorgflow\Situation;
+
+/**
+ * Retrieve an issue node from drupal.org.
+ */
+class DrupalOrgIssueNode implements FetcherInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fetchData(Situation $situation) {
+    // Set the user-agent for the request to drupal.org's API, to be polite.
+    // See https://www.drupal.org/api
+    ini_set('user_agent', "dorgpatch - https://github.com/joachim-n/dorgpatch.");
+
+    $issue_number = $situation->getIssueNumber();
+    $response = file_get_contents("https://www.drupal.org/api-d7/node/{$issue_number}.json");
+    $node = json_decode($response);
+
+    return $node;
+  }
+
+}
