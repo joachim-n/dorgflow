4b1ffa944ca49a675e0b6c39d8569cf8f62bc701
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-12-06 15:42:21 +0000

    user input DS

---
+++ DataSource/UserInput.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Dorgflow\DataSource;
+
+/**
+ * Extracts input data from user input.
+ */
+class UserInput extends DataSourceBase {
+
+  protected $issueNumber;
+
+  public function getIssueNumber() {
+    return $this->issueNumber;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function parse() {
+    global $argv;
+    if (!empty($argv[1])) {
+      if (is_numeric($argv[1])) {
+        $this->issueNumber = $argv[1];
+      }
+      else {
+        // If the param is a URL, get the node ID from the end of it.
+        $matches = [];
+        if (preg_match("@www\.drupal\.org/node/(?P<number>\d+)$@", $argv[1], $matches)) {
+          $this->issueNumber = $matches['number'];
+        }
+      }
+    }
+  }
+
+}
