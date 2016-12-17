c9eeafcd359489043984863c9346a6736ba5247a
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-12-17 15:15:34 +0000

    changed branch list to use plumbing

--- DataSource/Fetcher/GitBranchList.php
+++ DataSource/Fetcher/GitBranchList.php
@@ -4,6 +4,9 @@ namespace Dorgflow\DataSource\Fetcher;
 
 use Dorgflow\Situation;
 
+/**
+ * Gets a list of all git branches which are currently reachable.
+ */
 class GitBranchList implements FetcherInterface {
 
   /**
@@ -12,8 +15,22 @@ class GitBranchList implements FetcherInterface {
   public function fetchData(Situation $situation, $parameters) {
     // TODO: check in right dir!
 
-    // Get the branches that are reachable.
-    $branch_list = shell_exec("git branch --merged");
+    $branch_list = [];
+
+    // Get the list of local branches as 'SHA BRANCHNAME'.
+    $refs = shell_exec("git for-each-ref refs/heads/ --format='%(objectname) %(refname:short)'");
+    foreach (explode("\n", trim($refs)) as $line) {
+      list($sha, $branch_name) = explode(' ', $line);
+
+      $output = '';
+      // Exit value is 0 if true, 1 if false.
+      $return_var = '';
+      exec("git merge-base --is-ancestor $branch_name HEAD", $output, $return_var);
+
+      if ($return_var === 0) {
+        $branch_list[] = $branch_name;
+      }
+    }
 
     return $branch_list;
   }
