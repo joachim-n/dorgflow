fd2b8b3d15de1b0493aae70ce3fde7383f31f919
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-12-18 22:36:23 +0000

    inject situation and git into commands

--- dorgflow.php
+++ dorgflow.php
@@ -8,22 +8,30 @@ print "Hello, this is Dorgflow!\n";
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+// Helper objects to inject.
+$git = new \Dorgflow\Executor\Git();
+$situation = new \Dorgflow\Situation($git);
+// $analyser = new Analyser... ?
+
 // Figure out which command to run.
 if (empty($argv[1])) {
   // If we're run with no parameter, we're creating a patch.
-  $command = new Command\CreatePatch;
+  $command = new Command\CreatePatch($situation, $git);
 }
 else {
   if ($argv[1] == 'cleanup') {
-    $command = new Command\Cleanup;
+    $command = new Command\Cleanup($situation, $git);
   }
   elseif ($argv[1] == 'update') {
-    $command = new Command\LocalUpdate;
+    $command = new Command\LocalUpdate($situation, $git);
+  }
+  elseif ($argv[1] == 'test') {
+    $command = new Command\Test($situation, $git);
   }
   else {
     // If the parameter is something else, assume initial setup: the command
     // checks for a URL or issue number.
-    $command = new Command\LocalSetup;
+    $command = new Command\LocalSetup($situation, $git);
   }
 }
 
