25df7c98b3dd9d56e621bb531e6a0db4432c99ba
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-12-19 20:49:52 +0000

    TODO

--- DataSource/Fetcher/GitFeatureBranchLog.php
+++ DataSource/Fetcher/GitFeatureBranchLog.php
@@ -13,6 +13,8 @@ class GitFeatureBranchLog implements FetcherInterface {
     $master_branch_name = $situation->getMasterBranch()->getBranchName();
     $feature_branch_name = $situation->getFeatureBranch()->getBranchName();
 
+    // TODO! Complain if $feature_branch_name doesn't exist yet!
+
     $git_log = shell_exec("git rev-list {$feature_branch_name} ^{$master_branch_name} --pretty=oneline --reverse");
 
     return $git_log;
