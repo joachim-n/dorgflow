3e074477da3ff40be4a733bfb4ae65dc6d43cbaa
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-12-19 15:47:32 +0000

    removed current branch

--- DataSource/GitBranchList.php
+++ DataSource/GitBranchList.php
@@ -6,17 +6,8 @@ class GitBranchList extends DataSourceBase {
 
   protected $branchList = [];
 
-  protected $currentBranch;
-
   public function getBranchList() {
     return $this->branchList;
-  }
-
-  public function getCurrentBranch() {
-    // TODO: move to a new datasource, using:
-    // git symbolic-ref --short HEAD
-
-    return $this->currentBranch;
   }
 
 }
