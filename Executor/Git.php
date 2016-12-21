d0928e505e31933e28aa6a0813954d273182678f
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-12-21 21:59:44 +0000

    checkout files command - porcelain and plumbing

--- Executor/Git.php
+++ Executor/Git.php
@@ -20,7 +20,40 @@ class Git {
     }
   }
 
-  // checkoutFfiles =- see MasterBranch
+  /**
+   * Checks out the files of the given commit, without moving branches.
+   *
+   * This causes both the working directory and the staging to look like the
+   * files in the commit given by $treeish, without changing the actual commit
+   * or branch that git is currently on.
+   *
+   * The end result is that git has changes staged which take the current branch
+   * back to $treeish. The point of this is a patch which is against $treeish
+   * can now be applied.
+   *
+   * @param $treeish
+   *  A valid commit identifier, such as an SHA or branch name.
+   */
+  public function checkOutFiles($treeish) {
+    // Read the tree for the given commit into the index.
+    exec("git read-tree $treeish");
+
+    // Check out the index.
+    exec('git checkout-index -f -a');
+  }
+
+  // Porcelain version.
+  public function checkOutFilesPorcelain($treeish) {
+    $current_sha = shell_exec("git rev-parse HEAD");
+
+    // Reset the feature branch to the master branch tip commit. This puts the
+    // files in the same state as the master branch.
+    shell_exec("git reset --hard $treeish");
+
+    // Move the branch reference back to where it was, but without changing the
+    // files.
+    shell_exec("git reset --soft $current_sha");
+  }
 
   // apply patch file
 
