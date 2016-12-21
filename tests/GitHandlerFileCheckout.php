5b0c19b19e53ae9b51785e02005aa5bed2ecf321
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2016-12-21 22:00:04 +0000

    test

--- tests/GitHandlerFileCheckout.php
+++ tests/GitHandlerFileCheckout.php
@@ -15,10 +15,39 @@ class GitHandlerFileCheckout extends \PHPUnit_Framework_TestCase {
   protected function setUp() {
     chdir('Testing/repository');
     exec("git init .");
+    exec("git add main.txt");
+    exec("git commit -m 'Initial commit.'");
+
+    $b = shell_exec("git symbolic-ref --short -q HEAD");
+    dump($b);
   }
 
   public function testFileCheckout() {
     dump('hi');
+
+    $git = new \Dorgflow\Executor\Git;
+
+    $mock_situation = $this->createMock(\Dorgflow\Situation::class);
+
+    $patch_b_text = file_get_contents('patch-b.patch');
+    $patch_b = $this->getMockBuilder(\Dorgflow\Waypoint\Patch::class)
+      //->setConstructorArgs([$mock_situation])
+      ->disableOriginalConstructor()
+      ->setMethods(['getPatchFile'])
+      ->getMock();
+    $patch_b->method('getPatchFile')
+      ->willReturn($patch_b_text);
+
+    //$git->checkOutFiles('master');
+    $git->checkOutFilesPorcelain('master');
+
+    dump('apply patch.......');
+    $applied = $patch_b->applyPatchFile();
+    dump($applied);
+
+    if (!$applied) {
+      $this->fail();
+    }
   }
 
 
