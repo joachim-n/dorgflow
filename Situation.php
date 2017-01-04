10ad8d18d3653982d8747577c105bdb965087eec
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-01-04 21:56:08 +0000

    added docs; made commit message parsing public

--- Situation.php
+++ Situation.php
@@ -277,7 +277,21 @@ class Situation {
     return $patch_waypoints;
   }
 
-  protected function parseCommitMessage($message) {
+  /**
+   * Extract data from a commit message previously created by Dorgflow.
+   *
+   * @param $message
+   *  The message string.
+   *
+   * @return
+   *  Either FALSE if no data can be found in the message, or an array of data.
+   *  The following keys may be present:
+   *    - 'filename': The filename of the commit's patch.
+   *    - 'fid': The file entity ID. This will be absent in the case of a commit
+   *      made by the CreatePatch command, that is, for a patch the user is
+   *      creating to be uploaded to drupal.org.
+   */
+  public function parseCommitMessage($message) {
     // TODO: move this to the same sort of place as the creation as these
     // messages!
     $pattern_remote = "Patch from Drupal.org. File: (?P<filename>.+\.patch); fid (?P<fid>\d+). Automatic commit by dorgflow.";
