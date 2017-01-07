2a26dfce9580ff7dfe4a5969eb02f51eb585bc7f
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-01-07 14:18:06 +0000

    rough services definition

---
+++ services.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Services definition.
+ */
+
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\Definition;
+
+//$container
+//  ->register('waypoint_manager', '\Dorgflow\Executor\Git');
+  
+  
+$container->setDefinition('waypoint_manager', new Definition(
+    \Dorgflow\Service\WaypointManager::class,
+    ['git.info']
+));
+
+/*
+waypoint manager: git info...
+git info (logs etc): analyser (for the current branch), 
+analyser: d.org, git status
+d.org API (static caches things it fetches): git status
+git status (diff status and current branch)
+user input
+current folder
+git executor
+patch name handler
+*/
+
+$container
+  ->register('git.log', '\Dorgflow\Service\GitLog');
+
+$container
+  ->register('git.info', '\Dorgflow\Service\GitInfo');
+
+$container
+  ->register('git.executor', '\Dorgflow\Executor\Git');
+
+/*
+$container
+  ->register('newsletter_manager', 'NewsletterManager')
+  ->addMethodCall('setMailer', array(new Reference('mailer')));
+  
+*/
