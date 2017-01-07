9807cbedeacb6b641a95aec7fae31b9cf3280017
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-01-07 14:32:02 +0000

    Added getMasterBranch() to waypoint manager service.

--- Service/WaypointManager.php
+++ Service/WaypointManager.php
@@ -7,10 +7,18 @@ namespace Dorgflow\Service;
 // - situation ERM?
 class WaypointManager {
 
+  function __construct($git_info, $git_log, $git_executor) {
+    $this->git_info = $git_info;
+    $this->git_log = $git_log;
+    $this->git_executor = $git_executor;
+  }
+
   public function getMasterBranch() {
     if (empty($this->masterBranch)) {
       $this->masterBranch = new \Dorgflow\Waypoint\MasterBranch(
-      // !!!!!!!!!!
+        $this->git_info,
+        $this->git_log,
+        $this->git_executor
       );
     }
 
