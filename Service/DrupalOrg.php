92a03efe0bdd7e770f3509433ccd1371b67ac85e
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-01-09 21:06:23 +0000

    Added further methods to d.org service.

--- Service/DrupalOrg.php
+++ Service/DrupalOrg.php
@@ -3,7 +3,7 @@
 namespace Dorgflow\Service;
 
 /**
- * Retrieves data from drupal.org.
+ * Retrieves data from drupal.org's REST API.
  */
 class DrupalOrg {
 
@@ -13,16 +13,57 @@ class DrupalOrg {
     $this->analyser = $analyser;
   }
 
+  /**
+   * Returns the issue node title.
+   *
+   * @return string
+   *  The issue node title.
+   */
   public function getIssueNodeTitle() {
+    if (!isset($this->node_data)) {
+      $this->fetchIssueNode();
+    }
 
+    return $this->node_data->title;
   }
 
+  /**
+   * Returns the expected index for the next comment.
+   *
+   * @return int
+   *  The comment index for the next comment (assuming nobody else posts a
+   *  comment in the meantime!).
+   */
   public function getNextCommentIndex() {
+    if (!isset($this->node_data)) {
+      $this->fetchIssueNode();
+    }
 
+    $comment_count = $this->node_data->comment_count;
+    $next_comment_index = $comment_count + 1;
+    return $next_comment_index;
   }
 
+  /**
+   * Gets the field items for the issue files field, in order of creation.
+   *
+   * @return
+   *  An array of Drupal file field items.
+   */
   public function getIssueFileFieldItems() {
+    if (!isset($this->node_data)) {
+      $this->fetchIssueNode();
+    }
 
+    $files = $this->node_data->field_issue_files;
+
+    // Ensure these are in creation order by ordering them by fid.
+    // TODO: in due course, get the comment index data!!! -- see d.org issue!
+    usort($files, function($a, $b) {
+      return ($a->file->id <=> $b->file->id);
+    });
+
+    return $files;
   }
 
   public function getFileEntity() {
@@ -31,15 +72,17 @@ class DrupalOrg {
   public function getPatchFile() {
   }
 
+  /**
+   * Fetches the issue node from drupal.org's REST API.
+   */
   protected function fetchIssueNode() {
     $issue_number = $situation->getIssueNumber();
 
     print "Fetching node $issue_number from drupal.org.\n";
 
     $response = file_get_contents("https://www.drupal.org/api-d7/node/{$issue_number}.json");
-    $node = json_decode($response);
 
-    // return $node;
+    $this->node_data = json_decode($response);
   }
 
 }
