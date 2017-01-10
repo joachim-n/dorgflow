9f968f1801699ee82fe5e2bd0a47880170f22281
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-01-10 21:24:48 +0000

    Updated apply command to use injected services.

--- Command/Apply.php
+++ Command/Apply.php
@@ -2,25 +2,27 @@
 
 namespace Dorgflow\Command;
 
-use Dorgflow\Situation;
-
 /**
  * Applies the current feature branch to the master branch as a squash merge.
  */
 class Apply extends CommandBase {
 
   public function execute() {
-    $situation = $this->situation;
+    // TEMPORARY: get services from the container.
+    // @todo inject these.
+    $this->waypoint_manager_branches = $this->container->get('waypoint_manager.branches');
+    $this->git_executor = $this->container->get('git.executor');
+    $this->analyser = $this->container->get('analyser');
 
     // Check git is clean.
-    $clean = $situation->GitStatus()->gitIsClean();
+    $clean = $this->git_info->gitIsClean();
     if (!$clean) {
       throw new \Exception("Git repository is not clean. Aborting.");
     }
 
     // Create branches.
-    $master_branch = $situation->getMasterBranch();
-    $feature_branch = $situation->getFeatureBranch();
+    $master_branch = $this->waypoint_manager_branches->getMasterBranch();
+    $feature_branch = $this->waypoint_manager_branches->getFeatureBranch();
 
     // If the feature branch is not current, abort.
     if (!$feature_branch->exists()) {
@@ -36,17 +38,17 @@ class Apply extends CommandBase {
     // from d.org
 
     // Check out the master branch.
-    $this->git->checkOutBranch($master_branch->getBranchName());
+    $this->git_executor->checkOutBranch($master_branch->getBranchName());
     // Perform a squash merge from the feature branch: in other words, all the
     // changes on the feature branch are now staged on master.
-    $this->git->squashMerge($feature_branch->getBranchName());
+    $this->git_executor->squashMerge($feature_branch->getBranchName());
 
     print strtr("Changes from feature branch !feature-branch are now applied and staged on branch !master-branch.\n", [
       '!feature-branch' => $feature_branch->getBranchName(),
       '!master-branch'  => $master_branch->getBranchName(),
     ]);
     print strtr("You should now commit this, using the command from the issue on drupal.org: https://www.drupal.org/node/!id#drupalorg-issue-credit-form.\n", [
-      '!id' => $situation->getIssueNumber(),
+      '!id' => $this->analyser->deduceIssueNumber(),
     ]);
   }
 
