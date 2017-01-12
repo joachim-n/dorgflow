d55cf489375a2541bda4c4dba6769da7547c3184
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-01-12 23:18:38 +0000

    Changed update command to use static factory.

--- Command/LocalUpdate.php
+++ Command/LocalUpdate.php
@@ -2,16 +2,30 @@
 
 namespace Dorgflow\Command;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
 class LocalUpdate extends CommandBase {
 
-  public function execute() {
-    // TEMPORARY: get services from the container.
-    // @todo inject these.
-    $this->git_info = $this->container->get('git.info');
-    $this->waypoint_manager_branches = $this->container->get('waypoint_manager.branches');
-    $this->waypoint_manager_patches = $this->container->get('waypoint_manager.patches');
-    $this->git_executor = $this->container->get('git.executor');
+  /**
+   * Creates an instance of this command, injecting services from the container.
+   */
+  static public function create(ContainerBuilder $container) {
+    return new static(
+      $container->get('git.info'),
+      $container->get('waypoint_manager.branches'),
+      $container->get('waypoint_manager.patches'),
+      $container->get('git.executor')
+    );
+  }
 
+  function __construct($git_info, $waypoint_manager_branches, $waypoint_manager_patches, $git_executor) {
+    $this->git_info = $git_info;
+    $this->waypoint_manager_branches = $waypoint_manager_branches;
+    $this->waypoint_manager_patches = $waypoint_manager_patches;
+    $this->git_executor = $git_executor;
+  }
+
+  public function execute() {
     // Check git is clean.
     $clean = $this->git_info->gitIsClean();
     if (!$clean) {
