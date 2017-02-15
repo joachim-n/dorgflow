32f4936f21497e6f1e3f17e79d149737373c15a6
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-02-15 14:40:21 +0000

    Changed getCommitMessage() to use service; reverted to protected.

--- Waypoint/Patch.php
+++ Waypoint/Patch.php
@@ -148,15 +148,11 @@ class Patch {
   /**
    * Creates a commit message to use for the patch.
    *
-   * (Public just for testing.)
-   *
    * @return string
    *  The commit message.
    */
-  public function getCommitMessage() {
-    // TODO: include comment index!!!!!!!!!
-    $filename = $this->getPatchFilename();
-    return "Patch from Drupal.org. File: $filename; fid $this->fid. Automatic commit by dorgflow.";
+  protected function getCommitMessage() {
+    return $this->commit_message->createCommitMessage($this);
   }
 
   protected function makeGitCommit() {
