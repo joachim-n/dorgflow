ecfbe3cc8a92013a2747b443e3e1c271f2694857
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-02-22 11:44:48 +0000

    Added test case for update when feature branch isn't current.

--- tests/CommandLocalUpdateTest.php
+++ tests/CommandLocalUpdateTest.php
@@ -101,4 +101,65 @@ class CommandLocalUpdateTest extends \PHPUnit\Framework\TestCase {
     }
   }
 
+  /**
+   * Tests the case where the feature branch isn't current.
+   */
+  public function testNotOnFeatureBranch() {
+    $container = new \Symfony\Component\DependencyInjection\ContainerBuilder();
+
+    $git_info = $this->createMock(\Dorgflow\Service\GitInfo::class);
+    // Git is clean so the command proceeds.
+    $git_info->method('gitIsClean')
+      ->willReturn(TRUE);
+    $git_info->method('getBranchList')
+      ->willReturn([
+        // There is a feature branch.
+        '123456-terrible-bug',
+        '8.x-2.x' => 'sha',
+        'some-branch-name' => 'sha',
+        'something-else' => 'sha',
+      ]);
+    // Master branch is current rather than the feature branch.
+    $git_info->method('getCurrentBranch')
+      ->willReturn('8.x-2.x');
+    $container->set('git.info', $git_info);
+
+    // The analyser returns an issue number.
+    $analyser = $this->createMock(\Dorgflow\Service\Analyser::class);
+    $analyser->method('deduceIssueNumber')
+      ->willReturn(123456);
+    $container->set('analyser', $analyser);
+
+    // The git executor should not be called at all.
+    $git_executor = $this->createMock(\Dorgflow\Service\GitExecutor::class);
+    $git_executor->expects($this->never())->method($this->anything());
+    $container->set('git.executor', $git_executor);
+
+    // Drupal.org API should not be called at all.
+    $drupal_org = $this->createMock(\Dorgflow\Service\DrupalOrg::class);
+    $drupal_org->expects($this->never())->method($this->anything());
+    $container->set('drupal_org', $drupal_org);
+
+    // Need the real service for this, as we want the command to get the branch
+    // object from it, based on the mocked git.info service.
+    $container
+      ->register('waypoint_manager.branches', \Dorgflow\Service\WaypointManagerBranches::class)
+      ->addArgument(new Reference('git.info'))
+      ->addArgument(new Reference('drupal_org'))
+      ->addArgument(new Reference('git.executor'))
+      ->addArgument(new Reference('analyser'));
+
+    $container->set('waypoint_manager.patches', $this->getMockBuilder(\Dorgflow\Service\WaypointManagerPatches::class));
+
+    $command = \Dorgflow\Command\LocalUpdate::create($container);
+
+    try {
+      $command->execute();
+      $this->fail("The exception was not thrown.");
+    }
+    catch (\Exception $e) {
+      $this->assertTrue(TRUE, "The exception was thrown as expected.");
+    }
+  }
+
 }
