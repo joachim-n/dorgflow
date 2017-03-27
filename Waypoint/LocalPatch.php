3095a66f6d2f2373bbc986041863268cb474c21f
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-03-27 13:58:04 +0100

    Added method to local patch to get expected comment index.

--- Waypoint/LocalPatch.php
+++ Waypoint/LocalPatch.php
@@ -40,6 +40,25 @@ class LocalPatch {
   }
 
   /**
+   * Gets the (expected) comment index for the patch file.
+   *
+   * This is the number of the comment that will to the node when the file
+   * was added. Comment numbers start at 1 and increment for each comment.
+   * (Obviously, if another user comments on the issue in between this patch
+   * being created and the Dorgflow user uploading it, the number will be
+   * wrong.)
+   *
+   * @return int
+   *  The comment index.
+   */
+  public function getPatchFileIndex() {
+    if (!isset($this->index)) {
+      $this->index = $this->drupal_org->getNextCommentIndex();
+    }
+    return $this->index;
+  }
+
+  /**
    * Creates a filename for this waypoint's patch file.
    *
    * @return string
@@ -47,7 +66,7 @@ class LocalPatch {
    */
   protected function makePatchFilename() {
     $issue_number = $this->analyser->deduceIssueNumber();
-    $comment_number = $this->drupal_org->getNextCommentIndex();
+    $comment_number = $this->getPatchFileIndex();
     $patch_number = "$issue_number-$comment_number";
 
     $current_project = $this->analyser->getCurrentProjectName();
