618bde7567951ea5224dc0bb31de6ed84c37c603
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-04-21 08:06:06 +0100

    Added method to branch service to determine whether a feature branch is fully merged with master.

--- Service/WaypointManagerBranches.php
+++ Service/WaypointManagerBranches.php
@@ -42,4 +42,27 @@ class WaypointManagerBranches {
     return $this->feature_branch;
   }
 
+  /**
+   * Determines whether the feature branch is fully merged with master.
+   *
+   * @param \Dorgflow\Waypoint\FeatureBranch $feature_branch
+   *  The feature branch.
+   *
+   * @return bool
+   *  TRUE if the feature descends from master, that is, the tip of master is
+   *  reachable from the feature branch. FALSE otherwise, that is, if the
+   *  feature branch needs to be rebased.
+   */
+  public function featureBranchIsUpToDateWithMaster(FeatureBranch $feature_branch) {
+    if (!$feature_branch->exists()) {
+      // If the branch doesn't exist, then it's not up to date.
+      return FALSE;
+    }
+
+    return $this->git_info->isAncestor(
+      $this->getMasterBranch()->getBranchName(),
+      $this->getFeatureBranch()->getBranchName()
+    );
+  }
+
 }
