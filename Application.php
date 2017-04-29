b083871ed8018baa45a360b441e29471089207fc
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-04-29 07:43:17 +0100

    subclass Symphony Application

---
+++ Application.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Dorgflow;
+
+use Symfony\Component\Console\Application as SymfonyApplication;
+
+class Application extends SymfonyApplication {
+  
+  // TODO; override find() to essentiall give TWO defaults.
+  
+}
