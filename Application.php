a5674cdce77d0e4f3d4d9c8368cef621cd808d52
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-04-30 14:45:08 +0100

    more in app class

--- Application.php
+++ Application.php
@@ -5,7 +5,15 @@ namespace Dorgflow;
 use Symfony\Component\Console\Application as SymfonyApplication;
 
 class Application extends SymfonyApplication {
-  
-  // TODO; override find() to essentiall give TWO defaults.
-  
+
+  /**
+   * {@inheritdoc}
+   */
+  public function find($name) {
+    //dump($name);
+    // TODO: handle non-name first command to be the setup command
+
+    return parent::find($name);
+  }
+
 }
